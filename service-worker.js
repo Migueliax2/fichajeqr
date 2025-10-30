9d876084506d9bbd48936853d634139e1a782856
Miguel Carmona <mcarmonaiax2@gmail.com>
2025-10-30 20:16:19 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,34 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('fichajeqr-pwa').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
-  );
+const CACHE = "fichajeqr-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "https://unpkg.com/html5-qrcode"
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', function(e) {
+
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e)=>{
+  const req = e.request;
+  // Network-first for webhook posts; cache-first for assets
+  if(req.method !== "GET"){ return; }
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches.match(req).then(cached=> cached || fetch(req).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(req, copy));
+      return res;
+    }).catch(()=>cached))
   );
 });
